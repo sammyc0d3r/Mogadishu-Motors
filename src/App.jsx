74f6813dd84292c80e7aecf1e555c8e9229362f5
Abdisamad Omar <sammycoder@hotmail.com>
2024-07-31 13:35:58 +0300

    chore: Add Vite configuration files and update project structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from "react"
+import Navbar from "./assets/component/Navbar/Navbar"
+
+function App() {
+  return (
+    <div >
+      <Navbar/>
+
+    </div>
+    
+  )
+}
+
+export default App
